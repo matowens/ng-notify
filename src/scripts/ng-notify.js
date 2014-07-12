19071bac890bc91b0679d9fa3ebc5a23c4a52ebf
Mat <mat@peglegg.net>
2014-07-12 01:46:00 -0500

    Added new sticky notification functionality and styles.  Also updates on code and comment organization.

--- src/scripts/ng-notify.js
+++ src/scripts/ng-notify.js
@@ -1,5 +1,5 @@
 /**
- * @license ng-notify v0.3.0
+ * @license ng-notify v0.4.0
  * http://matowens.github.io/ng-notify
  * (c) 2014 MIT License, matowens.com
  */
@@ -27,7 +27,8 @@
                     theme: 'pure',
                     position: 'bottom',
                     duration: 3000,
-                    type: 'info'
+                    type: 'info',
+                    sticky: false
                 };
 
                 // Options...
@@ -60,100 +61,14 @@
                 // Template and scope...
 
                 var notifyScope = $rootScope.$new();
-                var tpl = $compile('<div class="ngn" ng-class="ngNotify.notifyClass">{{ ngNotify.notifyMessage }}</div>')(notifyScope);
+                var tpl = $compile(
+                    '<div class="ngn" ng-class="ngNotify.notifyClass">' +
+                        '<span class="ngn-dismiss" ng-click="dismiss()">&times;</span>' +
+                        '<span>{{ ngNotify.notifyMessage }}</span>' +
+                    '</div>'
+                )(notifyScope);
 
                 $document.find('body').append(tpl);
-
-                // Public methods...
-
-                var notifyObject = {
-
-                    /**
-                     * Merges our user specified options with our default set of options.
-                     * 
-                     * @param {Object} params - object of user provided options to configure notifications.
-                     */
-                    config: function(params) {
-                        params = params || {};
-                        angular.extend(options, params);
-                    },
-
-                    /**
-                     * Sets, configures and displays each notification.
-                     * 
-                     * @param {String}             message - the message our notification will display to the user.
-                     * @param {String|Object|null} userOpt - optional parameter that contains the type or an object of options used to configure this notification. 
-                     */
-                    set: function(message, userOpt) {
-
-                        if (!message) {
-                            return;
-                        }
-
-                        $interval.cancel(notifyInterval);
-                        $timeout.cancel(notifyTimeout);
-
-                        var userOpts = {};
-
-                        if(typeof userOpt === 'object') {
-                            userOpts = {
-                                type: userOpt.type || undefined,
-                                theme: userOpt.theme || undefined,
-                                position: userOpt.position || undefined,
-                                duration: userOpt.duration || undefined
-                            };
-                        } else {
-                            userOpts.type = userOpt;
-                        }
-
-                        var notifyClass = setType(userOpts.type) + ' ' + 
-                                          setTheme(userOpts.theme) + ' ' +
-                                          setPosition(userOpts.position);
-                        var duration = setDuration(userOpts.duration);
-
-                        notifyScope.ngNotify = {
-                            notifyClass: notifyClass,
-                            notifyMessage: message
-                        };
-
-                        var el = fadeLib(tpl);
-
-                        el.fadeIn(200, function() {
-                            notifyTimeout = $timeout(function() {
-                                el.fadeOut(500, function() {
-                                    notifyReset();
-                                });
-                            }, duration);
-                        });
-                    },
-
-                    // User customizations...
-
-                    /**
-                     * Adds a new, user specified theme to our notification system 
-                     * that they can then use throughout their application.
-                     * 
-                     * @param {String} themeName  - the name for this new theme that will be used when applying it via configuration.
-                     * @param {String} themeClass - the class that this theme will use when applying it's styles.
-                     */
-                    addTheme: function(themeName, themeClass) {
-                        if(!themeName || !themeClass) { return; }
-                        themes[themeName] = themeClass;
-                    },
-
-                    /**
-                     * Adds a new, user specified notification type that they
-                     * can then use throoughout their application.
-                     * 
-                     * @param {String} typeName  - the name for this new type that will be used when applying it via configuration.
-                     * @param {String} typeClass - the class that this type will use when applying it's styles.
-                     */
-                    addType: function(typeName, typeClass) {
-                        if(!typeName || !typeClass) { return; }
-                        types[typeName + 'Class'] = typeClass;
-                    }
-
-                };
 
                 // Private methods...
 
@@ -199,6 +114,16 @@
                 var setDuration = function(providedDuration) {
                     var duration = providedDuration || options.duration;
                     return angular.isNumber(duration) ? duration : 3500;
+                };
+
+                /**
+                 * Sets our notification's sticky state, forcing the user to dismiss it when enabled.
+                 * 
+                 * @param {Bool} providedSticky - boolean on whether or not sticky state should be enabled.
+                 */
+                var setSticky = function(providedSticky) {
+                    var sticky = providedSticky || options.sticky;
+                    return sticky ? true : false;
                 };
 
                 /**
@@ -255,14 +180,14 @@
                         el.css('filter', 'progid:DXImageTransform.Microsoft.Alpha(Opacity=' + opacity * 100 + ')'); // IE8
                         el.css('opacity', opacity);
 
-                        if(opacity <= 0 || opacity >= 1) {
+                        if (opacity <= 0 || opacity >= 1) {
                             $interval.cancel(notifyInterval);
 
-                            if(opacity <= 0) {
+                            if (opacity <= 0) {
                                 el.css('display', 'none');
                             }
 
-                            if(callback) { 
+                            if (callback) { 
                                 callback(); 
                             }
                         }
@@ -291,6 +216,119 @@
                  */
                 fadeLib.fn.prototype.fadeOut = function(duration, callback) {
                     this._fade(-1, 1, duration, callback);
+                };
+
+                /**
+                 * Dismisses our notification when called, attached to scope for ngCLick event to trigger.
+                 * 
+                 */
+                notifyScope.dismiss = function() {
+                    el.fadeOut(500, function() {
+                        notifyReset();
+                    });
+                };
+
+                /**
+                 * Our template bound to it's own personal scope and receiving our fade protoype functions.
+                 * 
+                 * @type {Object}
+                 */
+                var el = fadeLib(tpl);
+
+                /**
+                 * Our primary object containing all public API methods and allows for all our functionality to be invoked.
+                 * 
+                 * @type {Object}
+                 */
+                var notifyObject = {
+
+                    /**
+                     * Merges our user specified options with our default set of options.
+                     * 
+                     * @param {Object} params - object of user provided options to configure notifications.
+                     */
+                    config: function(params) {
+                        params = params || {};
+                        angular.extend(options, params);
+                    },
+
+                    /**
+                     * Sets, configures and displays each notification.
+                     * 
+                     * @param {String}             message - the message our notification will display to the user.
+                     * @param {String|Object|null} userOpt - optional parameter that contains the type or an object of options used to configure this notification. 
+                     */
+                    set: function(message, userOpt) {
+
+                        if (!message) {
+                            return;
+                        }
+
+                        $interval.cancel(notifyInterval);
+                        $timeout.cancel(notifyTimeout);
+
+                        var userOpts = {};
+
+                        if (typeof userOpt === 'object') {
+                            userOpts = {
+                                type: userOpt.type || undefined,
+                                theme: userOpt.theme || undefined,
+                                position: userOpt.position || undefined,
+                                duration: userOpt.duration || undefined,
+                                sticky: userOpt.sticky || undefined
+                            };
+                        } else {
+                            userOpts.type = userOpt;
+                        }
+
+                        var sticky = setSticky(userOpts.sticky);
+                        var duration = setDuration(userOpts.duration);
+                        var notifyClass = setType(userOpts.type) + ' ' + 
+                                          setTheme(userOpts.theme) + ' ' +
+                                          setPosition(userOpts.position);
+
+                        notifyClass += sticky ? ' ngn-sticky' : '';
+
+                        notifyScope.ngNotify = {
+                            notifyClass: notifyClass,
+                            notifyMessage: message
+                        };
+
+                        el.fadeIn(200, function() {
+                            if (!sticky) {
+                                notifyTimeout = $timeout(function() {
+                                    notifyScope.dismiss();
+                                }, duration);
+                            }
+                        });
+                    },
+
+                    // User customizations...
+
+                    /**
+                     * Adds a new, user specified theme to our notification system 
+                     * that they can then use throughout their application.
+                     * 
+                     * @param {String} themeName  - the name for this new theme that will be used when applying it via configuration.
+                     * @param {String} themeClass - the class that this theme will use when applying it's styles.
+                     */
+                    addTheme: function(themeName, themeClass) {
+                        if (!themeName || !themeClass) { return; }
+                        themes[themeName] = themeClass;
+                    },
+
+                    /**
+                     * Adds a new, user specified notification type that they
+                     * can then use throoughout their application.
+                     * 
+                     * @param {String} typeName  - the name for this new type that will be used when applying it via configuration.
+                     * @param {String} typeClass - the class that this type will use when applying it's styles.
+                     */
+                    addType: function(typeName, typeClass) {
+                        if (!typeName || !typeClass) { return; }
+                        types[typeName + 'Class'] = typeClass;
+                    }
+
                 };
 
                 return notifyObject;
